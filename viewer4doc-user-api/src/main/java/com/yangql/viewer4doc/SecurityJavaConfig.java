4b094e6c1a58b6dd574721a472859073965f0051
yangjae33 <wogurrhtn@gmail.com>
2020-09-16 18:45:17 +0900

    FIX: solve Swagger-ui access Error

--- viewer4doc-user-api/src/main/java/com/yangql/viewer4doc/SecurityJavaConfig.java
+++ viewer4doc-user-api/src/main/java/com/yangql/viewer4doc/SecurityJavaConfig.java
@@ -17,14 +17,26 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     @Value("${jwt.secret}")
     private String secret;
 
+    private static final String[] AUTH_WHITELIST = {
+
+            // -- swagger ui
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .antMatchers("/**/*").denyAll();
         http
-
                 .cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
                 .headers().frameOptions().disable();
+
     }
 
     @Bean
