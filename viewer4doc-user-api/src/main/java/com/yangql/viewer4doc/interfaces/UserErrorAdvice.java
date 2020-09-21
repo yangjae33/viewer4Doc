da5f4a9295f6bb3157dc2f7da267a2fe2f2c0c03
yangjae33 <wogurrhtn@gmail.com>
2020-09-21 19:34:52 +0900

    UPDATE : user-api API docs 수정

--- viewer4doc-user-api/src/main/java/com/yangql/viewer4doc/interfaces/UserErrorAdvice.java
+++ viewer4doc-user-api/src/main/java/com/yangql/viewer4doc/interfaces/UserErrorAdvice.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.HashMap;
+
 @ControllerAdvice
 public class UserErrorAdvice {
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(EmailExistedException.class)
     public String handleEmailNotExisted(){
-        return "{}";
+        return "Existed Email";
     }
 }
