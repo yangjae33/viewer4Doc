9d929ce6ae8be3316c82b8c6ac74e8e51e1be221
yangjae33 <wogurrhtn@gmail.com>
2020-09-21 14:53:46 +0900

    UPDATE : mvc모듈 cross origin

--- viewer4doc-mvc/src/main/java/com/yangql/viewer4doc/interfaces/ViewerController.java
+++ viewer4doc-mvc/src/main/java/com/yangql/viewer4doc/interfaces/ViewerController.java
@@ -1,9 +1,11 @@
 package com.yangql.viewer4doc.interfaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin
 @Controller
 public class ViewerController {
 
