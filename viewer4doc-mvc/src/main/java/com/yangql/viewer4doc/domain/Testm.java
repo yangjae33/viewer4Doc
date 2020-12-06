1e5c0f79573918595a9de6b938bb683c533482cc
yangjae33 <wogurrhtn@gmail.com>
2020-12-06 20:48:13 +0900

    [add] querydsl

---
+++ viewer4doc-mvc/src/main/java/com/yangql/viewer4doc/domain/Testm.java
@@ -0,0 +1,30 @@
+package com.yangql.viewer4doc.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Testm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+
+    @Builder
+    public Testm(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
---
+++ viewer4doc-mvc/src/main/java/com/yangql/viewer4doc/domain/TestmRepository.java
@@ -0,0 +1,6 @@
+package com.yangql.viewer4doc.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestmRepository extends JpaRepository<Testm,Long> {
+}
